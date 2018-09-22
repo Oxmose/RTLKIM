db72552a342953cc13ab1f96c5a2b78a17736191
Oxmose <alexy.torresa@gmail.com>
2018-09-22 10:37:01 -0400

    Added full documentation to config.h, fixed cpu_settings.h header.

--- Source/Config/config.h
+++ Source/Config/config.h
@@ -1,35 +1,64 @@
-/*******************************************************************************
- * File: config.h
+/***************************************************************************//**
+ * @file config.h
  *
- * Original author: Alexy Torres Aurora Dugo
+ * @see loader.S
  *
- * Created: 09/03/2018
+ * @author Alexy Torres Aurora Dugo
+ *
+ * @date 09/03/2018
+ *
+ * @version 2.0
+ *
+ * @brief Kernel's main configuration file.
  * 
- * Last author: Alexy Torres Aurora Dugo
+ * @details Kernel configuration's definitions. This file stores the different 
+ * settings used when compiling RTLK.
  * 
- * Last modified: 21/09/2018
- *
- * Version: 2.0
- *
- * Kernel configuration's definitions. This file stores the different settings 
- * used when compiling RTLK.
+ * @copyright Alexy Torres Aurora Dugo
  ******************************************************************************/
 
 #ifndef __CONFIG_H_
 #define __CONFIG_H_
 
-#define KERNEL_STACK_SIZE 16384 /* DO NOT FORGET TO MODIFY IN LOADER.S */
+/** 
+ * @brief Kernel's main stack size in bytes.
+ * 
+ * @warning When modifying this value, do not forget to modify it in the 
+ * loader.S file.
+ */
+#define KERNEL_STACK_SIZE 16384
+
+/** @brief Maximal number of CPU to be supported by the kernel. */
 #define MAX_CPU_COUNT     1
 
+/** @brief Enables SMP support. */
 #define ENABLE_SMP        0
+
+/** @brief Enables VESA support for graphic drivers. */
 #define ENABLE_VESA       0
+
+/** @brief Enables ATA drivers support. */
 #define ENABLE_ATA        0
+
+/** @brief Enables GUI support. */
 #define ENABLE_GUI        0
+
+/** @brief Enables mouse driver support. */
 #define ENABLE_MOUSE      0
 
 /* Screen settings */
+
+/** @brief When VESA drivers are enabled, defines the maximal supported height
+ * resolution.
+ */
 #define MAX_SUPPORTED_HEIGHT 1080
+/** @brief When VESA drivers are enabled, defines the maximal supported width
+ * resolution.
+ */
 #define MAX_SUPPORTED_WIDTH  1100
+/** @brief When VESA drivers are enabled, defines the maximal supported color
+ * depth.
+ */
 #define MAX_SUPPORTED_BPP    32
 
 #endif /* __CONFIG_H_ */
