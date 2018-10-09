e7b37a3ef587fa1eefdce373e4c69c345a6be9a3
Oxmose <alexy.torresa@gmail.com>
2018-10-09 18:04:02 -0400

    Added simple clock example

---
+++ Source/Examples/Clock/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+dir=$PWD
+cp main.c ../../Src/User/main.c
+cd ../../
+make && make run
+cd $dir
+echo "/* Used as example, it will be changed in the future. */" > ../../Src/User/main.c
+echo "int main(void)" >> ../../Src/User/main.c
+echo "{"  >> ../../Src/User/main.c
+echo "    return 0;"  >> ../../Src/User/main.c
+echo "}" >> ../../Src/User/main.c
