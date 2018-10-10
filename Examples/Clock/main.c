ded70ab01ca5ea2a901668aef7beb13b72bcbd57
Oxmose <alexy.torresa@gmail.com>
2018-10-09 21:37:00 -0400

    Fixed codacy error and added drive image

--- Examples/Clock/main.c
+++ Examples/Clock/main.c
@@ -5,17 +5,17 @@
 /* Used as example, it will be changed in the future. */
 int main(void)
 {    
-    uint32_t time, hours, minutes, seconds;
-    date_t date;
     printf("\n");
     while(1)
     {
+        date_t date;
+        uint32_t time, hours, minutes, seconds;
         time = rtc_get_current_daytime();
         hours = time / 3600;
         minutes = (time / 60) % 60;
         seconds = time % 60;
         date = rtc_get_current_date();
-        printf("\rDate: %02d/%02d/%02d | Time: %02d:%02d:%02d", date.day, date.month, date.year, hours, minutes, seconds);
+        printf("\rDate: %02u/%02u/%02u | Time: %02u:%02u:%02u", date.day, date.month, date.year, hours, minutes, seconds);
         sched_sleep(200);
     }
     return 0;
